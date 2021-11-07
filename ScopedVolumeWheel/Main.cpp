36c18a154e9788f0abaa1d04c69ec83938e436f1
Kyle Scheuing <kyle@kylescheuing.com>
2021-11-07 04:09:49 -0500

    Move project into subdirectory

--- ScopedVolumeWheel/Main.cpp
+++ ScopedVolumeWheel/Main.cpp
@@ -62,13 +62,14 @@ int main()
     }
 
     CoUninitialize();
+    return 0;
 }
 
-int WinMain(
-    HINSTANCE hInstance,
-    HINSTANCE hPrevInstance,
-    LPSTR     lpCmdLine,
-    int       nShowCmd
+int WINAPI WinMain(
+    _In_ HINSTANCE hInstance,
+    _In_opt_ HINSTANCE hPrevInstance,
+    _In_ LPSTR     lpCmdLine,
+    _In_ int       nShowCmd
 ) {
-    main();
+    return main();
 }
